9f39a1ccf65bead9ff3f50fc36ce50e93d43887e
wangxuegang <wangxuegang@dycd.com>
2017-10-27 11:45:36 +0800

    thinkPHP5

---
+++ thinkphp/runtime/temp/7e7a1c161ef0eee4827657744fbb22a7.php
@@ -0,0 +1,4 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:97:"C:\Users\Administrator\Desktop\www\work\thinkphp\public/../application/book\view\index\index.html";i:1508913593;}*/ ?>
+<div style="float: none">
+index.html
+</div>
